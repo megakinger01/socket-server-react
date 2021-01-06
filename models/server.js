2582cc2b77e6c7cdcab91928bf7cfdb421edf0b1
Pedro Ramirez <pedroramirezgio01@gmail.com>
2021-01-06 10:39:13 -0400

    act final

--- models/server.js
+++ models/server.js
@@ -4,6 +4,7 @@ const http  = require('http')
 const socketio =  require('socket.io')
 const path = require('path');
 const Sockets = require('./sockets');
+const cors = require('cors')
 
 
 class Server {
@@ -25,6 +26,8 @@ class Server {
     middlewares(){
         // DESPLEGAR EL DIRECTORIO PUBLICO
         this.app.use( express.static( path.resolve( __dirname , '../public') ))
+
+        this.app.use( cors() )
     }
 
     configurarSockets() {
